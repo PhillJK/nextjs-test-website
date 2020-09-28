1240b41d03392181f9ffe3ab50bf7a0ff1300a3e
PhillJK <farrux2315@gmail.com>
2020-09-28 14:12:31 +0500

    Installed json-server and concurrently. Created Mock database

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,18 @@
+import { useState, useEffect } from "react";
 import MainLayout from "../../layouts/MainLayout";
 
 const PostPage = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    async function load() {
+      const response = await fetch("https://localhost:4200/posts");
+      const json = response.json;
+      setPosts(json);
+    }
+  }, [posts]);
   return (
     <MainLayout title="Posts">
-      <h1>Post</h1>
+      <h1>Posts Page</h1>
     </MainLayout>
   );
 };
